a6d8f666856f96ce9b19fc55a4b31997df0ab8bd
Kyle <kylewscott512@gmail.com>
2023-12-01 13:45:17 -0500

    failing

--- test_math_functions.py
+++ test_math_functions.py
@@ -8,14 +8,14 @@ def test_calc_subtraction():
     output = subtract_numbers(2,4)
     assert output == -2
 
-#def test_calc_multiply():
-#    output = multiply_numbers(2,4)
-#    assert output == 8
+def test_calc_multiply():
+    output = multiply_numbers(2,4)
+    assert output == 8
 
-#def test_calc_multiply_fail():
-#    output = multiply_numbers(2,4)
-#    assert output == 16
+def test_calc_multiply_fail():
+    output = multiply_numbers(2,4)
+    assert output == 16
 
-#def test_calc_divide():
-#    output = divide_numbers(10,2)
-#   assert output == 5
+def test_calc_divide():
+    output = divide_numbers(10,2)
+    assert output == 5
